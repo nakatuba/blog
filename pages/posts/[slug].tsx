4799eb32b246635410b5fd1e31ed371fdc3de300
nakatuba <nakatuba0626@gmail.com>
2022-08-01 06:50:58 +0900

    Enable syntax highlight

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -2,6 +2,8 @@ import path from 'path'
 import Head from 'next/head'
 import Image from 'next/image'
 import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { okaidia } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 import { getPostBySlug, getAllPosts } from '../../lib/posts'
 import styles from '../../styles/Home.module.css'
 
@@ -30,6 +32,23 @@ export default function Post({ post }: Props) {
                 src={images.keys().includes(imagePath) ? images(imagePath) : ''}
                 alt={alt}
               />
+            )
+          },
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className ?? '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={okaidia}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
             )
           },
         }}
