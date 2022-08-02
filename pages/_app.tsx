fa7acd35791d19251ebcebac0f194dea959633c1
nakatuba <nakatuba0626@gmail.com>
2022-08-03 03:29:01 +0900

    Add header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Header from '../components/header'
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
@@ -5,6 +6,7 @@ import type { AppProps } from 'next/app'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Header />
       <Component {...pageProps} />
     </ChakraProvider>
   )
