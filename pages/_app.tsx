7d6349d19bb63972fedcf250cd0976bdb6d72b5d
nakatuba <nakatuba0626@gmail.com>
2022-08-03 08:16:29 +0900

    Fix footer position

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import Footer from '../components/footer'
 import Header from '../components/header'
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
+      <Flex flexDirection="column" minH="100vh">
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </Flex>
     </ChakraProvider>
   )
 }
