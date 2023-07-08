baa5616c88beacab63d2424f1c4b6895d96ffd93
nakatuba <nakatuba0626@gmail.com>
2023-07-09 03:33:05 +0900

    Remove unnecessary files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import Footer from '../components/footer'
 import Header from '../components/header'
-import '../styles/globals.css'
 import { ChakraProvider, Container, Flex } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
